3d2b20116cc5029c211c46c25201c2c972cdeeed
himel9675 <himel9675@7KSM6Y1.Student.UGDSB.ED>
2018-02-28 11:21:28 -0500

    This is the repository for the BillOfSale

---
+++ src/billofsale/BillOfSale.java
@@ -0,0 +1,29 @@
+/* By Hildana Melaku
+ * BillOfSale.java
+ * This program calculates the purchased price, total, taxes, 
+ * and change of a shirt
+ * Feb/27/2018 
+ */
+
+package billofsale;
+
+
+public class BillOfSale {
+
+
+    public static void main(String[] args) {
+        
+        double shirt = 12.49;
+        int bill = 20;
+        double tax = 0.13;
+        double totalPrice = shirt+(tax*shirt); //the price of the shirt with tax
+        double change = bill - totalPrice; //the change 
+        
+        //prints the answers 
+        System.out.print("price of shirt = " + shirt + "$" + "\nbill used = " + bill + "$");
+        System.out.print("\ntax = " + tax + "\ntotal price = " + totalPrice + "$");
+        System.out.print("\nchange = " + change + "$" );
+     
+    }
+    
+}
